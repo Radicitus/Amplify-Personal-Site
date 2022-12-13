d5c919eb2125ab3fd7328af27594f237ffe29164
Radicitus <novivolatus@gmail.com>
2022-12-13 00:04:29 -0800

    Small fixes

--- cam-personal-site/pages/index.tsx
+++ cam-personal-site/pages/index.tsx
@@ -86,14 +86,14 @@ export default function Home() {
 
           <div
             className={
-              "relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96"
+              "relative mx-auto bg-slate-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96"
             }
           >
             <Image
               src={profile}
               layout={"fill"}
-              objectFit={"cover"}
               alt={"Profile picture of Cameron"}
+              className={"object-cover"}
             />
           </div>
         </section>
