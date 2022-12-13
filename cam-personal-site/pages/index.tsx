04fda1ed2ca12518afc5930c063168544c4afdd5
Radicitus <novivolatus@gmail.com>
2022-12-12 21:53:25 -0800

    Add public files

--- cam-personal-site/pages/index.tsx
+++ cam-personal-site/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
+    <div>
+      <Head>
+        <title>Cameron Sherry Portfolio</title>
+        <meta name={"description"} content={"A little bit of Cam!"} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
 }
